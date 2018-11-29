62b69b868d0b8959d6dcc29fcba62c774973b696
Felipe Lorenzoni <florenzoni@brivia.com.br>
2018-11-29 16:46:15 -0200

    first version of project

---
+++ app/src/main/java/br/com/felorenzoni/agenda/ListaAlunoActivity.java
@@ -0,0 +1,13 @@
+package br.com.felorenzoni.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_aluno);
+    }
+}
