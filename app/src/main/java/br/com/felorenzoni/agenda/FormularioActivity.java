23436cfc5bf5d99b2fd8c724615708dc7c44162d
Felipe Lorenzoni <florenzoni@brivia.com.br>
2018-12-14 17:50:19 -0200

    Remove btn Salvar, adiciona menu para salvar

--- app/src/main/java/br/com/felorenzoni/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/felorenzoni/agenda/FormularioActivity.java
@@ -2,11 +2,12 @@ package br.com.felorenzoni.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-import java.text.Normalizer;
+
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -15,19 +16,27 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        Button btnSalvar =  findViewById(R.id.formulario_btn_salvar);
-
-
-        btnSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(FormularioActivity.this, "aluno Salvo com Sucesso", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
 
-
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok:
+                Toast.makeText(FormularioActivity.this, "aluno Salvo com Sucesso", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
